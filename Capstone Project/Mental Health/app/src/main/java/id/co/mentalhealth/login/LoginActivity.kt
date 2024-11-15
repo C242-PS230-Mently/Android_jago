7164ef1722bf43e69e71cb065286c8ef6c9d6b53
Raihanith <godlovesus.ae7@gmail.com>
2024-11-15 14:21:26 +0800

    menambahkan LoginActivity

--- Capstone Project/Mental Health/app/src/main/java/id/co/mentalhealth/login/LoginActivity.kt
+++ Capstone Project/Mental Health/app/src/main/java/id/co/mentalhealth/login/LoginActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import id.co.mentalhealth.R
+import id.co.mentalhealth.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
