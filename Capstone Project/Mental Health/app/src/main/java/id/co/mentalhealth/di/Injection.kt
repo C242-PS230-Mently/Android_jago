29fc20e1ef90fc5b819c7a36f436659cb873f966
Raihanith <godlovesus.ae7@gmail.com>
2024-11-29 11:05:58 +0800

    Make Interceptor in ApiConfig, and add token to Injection

--- Capstone Project/Mental Health/app/src/main/java/id/co/mentalhealth/di/Injection.kt
+++ Capstone Project/Mental Health/app/src/main/java/id/co/mentalhealth/di/Injection.kt
@@ -5,11 +5,14 @@ import id.co.mentalhealth.data.pref.UserPreferences
 import id.co.mentalhealth.data.pref.dataStore
 import id.co.mentalhealth.data.network.retrofit.ApiConfig
 import id.co.mentalhealth.ui.auth.AuthRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 object Injection {
     fun provideRepository(context: Context): AuthRepository {
-        val apiService = ApiConfig.getApiService()
         val pref = UserPreferences.getInstance(context.dataStore)
+        val user = runBlocking { pref.getSession().first() }
+        val apiService = ApiConfig.getApiService(user.token)
         return AuthRepository.getInstance(apiService, pref)
     }
 }
